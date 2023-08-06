635d22318399e7ab84a2970324948c73f01cc796
Rico Otto <anton@brewrmarketing.nl>
2023-08-07 01:46:40 +0200

    push favicon and fix pricing

--- personal/src/lib/components/TweetSummary/TweetSummary.svelte
+++ personal/src/lib/components/TweetSummary/TweetSummary.svelte
@@ -1,18 +1,5 @@
 <script>
-    let tweets = [
-    {
-        "date": "12-07-2023",
-        "link": "https://twitter.com/AntonOtto_FFC/status/1679213807552323584?s=20",
-        "title": "12 of my Mantras",
-        "description": "12 of my Mantras YOU SHOULD not only Recite but try to understand and integrate in your life!"
-    },
-    {
-        "date": "22-07-2023",
-        "link": "https://twitter.com/AntonOtto_FFC/status/1682515996739772418?s=20",
-        "title": "Let me share you a story.",
-        "description": "Let me share you a story.  About me.  Lately I get some questions from people about how I do so much.  And what motivates me.  Is it my daughters?  Is it something else? [...]"
-    },
-    ]
+    export let tweets;
 </script>
 
 <section>
--- personal/src/routes/+page.svelte
+++ personal/src/routes/+page.svelte
@@ -11,7 +11,34 @@
     let name = person.name;
     let subtitle = person.subtitle;
     let website = person.website;
+
+
+
+    let tweets = [
+    {
+        "date": "12-07-2023",
+        "link": "https://twitter.com/AntonOtto_FFC/status/1679213807552323584?s=20",
+        "title": "12 of my Mantras",
+        "description": "12 of my Mantras YOU SHOULD not only Recite but try to understand and integrate in your life!"
+    },
+    {
+        "date": "22-07-2023",
+        "link": "https://twitter.com/AntonOtto_FFC/status/1682515996739772418?s=20",
+        "title": "Let me share you a story.",
+        "description": "Let me share you a story.  About me.  Lately I get some questions from people about how I do so much.  And what motivates me.  Is it my daughters?  Is it something else? [...]"
+    },
+    {
+        "date": "07-08-2023",
+        "link": "https://twitter.com/AntonOtto_FFC/status/1687913501619044352?s=20",
+        "title": "Monetization is an important part of creating a sustainable business 🤑",
+        "description": "There is 101 ways to monetize your skills and knowledge   But these are my favorites: [...]"
+    }
+
+
+   
+    ]
 </script>
 
 <Header {name} {subtitle} {website} />
-<TweetSummary />
+<TweetSummary {tweets} />
+
